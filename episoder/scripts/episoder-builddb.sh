c9f055a0464eb38281d98968085d045d9e6b16e2
Stefan Ott <stefan@ott.net>
2006-04-19 13:13:26 +0000

    *** empty log message ***

--- episoder/scripts/episoder-builddb.sh
+++ episoder/scripts/episoder-builddb.sh
@@ -1,6 +1,6 @@
 # episoder-builddb.sh, http://episoder.sourceforge.net/
 #
-# Copyright (c) 2004, 2005 Stefan Ott. All rights reserved.
+# Copyright (c) 2004-2006 Stefan Ott. All rights reserved.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
