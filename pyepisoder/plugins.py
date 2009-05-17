fad1b2543ea3ff7c4ff42f1498493e6a3599c3fa
Stefan Ott <stefan@ott.net>
2009-05-17 16:00:30 +0000

    Added setup.py

--- pyepisoder/plugins.py
+++ pyepisoder/plugins.py
@@ -1,5 +1,24 @@
-import os
+# episoder, http://episoder.sourceforge.net/
+#
+# Copyright (C) 2004-2009 Stefan Ott. All rights reserved.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
 import re
+import os
+import sys
 import yaml
 import logging
 import urllib2
@@ -47,7 +66,8 @@ class DummyParser(object):
 class EpguidesParser(object):
 	def __init__(self):
 		self.logger = logging.getLogger('EpguidesParser')
-		self.awkfile = '/home/stefan/projects/episoder/branches/0.5/extras/episoder_helper_epguides.awk'
+		self.awkfile = os.path.join(sys.prefix, "share", "episoder",
+				"episoder_helper_epguides.awk")
 		self.awk = '/usr/bin/awk'
 
 	def __str__(self):
