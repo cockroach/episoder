a989e2d86f26b91e5e892a7e166fc0a0fb571264
Stefan Ott <stefan@ott.net>
2009-08-25 05:48:09 +0000

    Cleanup

--- test/test.py
+++ test/test.py
@@ -10,7 +10,7 @@ import datetime
 import pyepisoder.episoder as episoder
 import pyepisoder.plugins as plugins
 
-class TestDataStore(unittest.TestCase):
+class DataStoreTest(unittest.TestCase):
 	def setUp(self):
 		self.path = tempfile.mktemp()
 		self.store = episoder.DataStore(self.path)
@@ -325,6 +325,47 @@ class TestDataStore(unittest.TestCase):
 		self.store.addEpisode(episode4)
 		self.store.addEpisode(episode5)
 		self.assertEqual(2, len(self.store.getEpisodes()))
+
+class ShowTest(unittest.TestCase):
+	def setUp(self):
+		self.path = tempfile.mktemp()
+		self.store = episoder.DataStore(self.path)
+		self.show = episoder.Show('A', url='a')
+		self.show = self.store.addShow(self.show)
+
+	def tearDown(self):
+		os.unlink(self.path)
+
+	def testRemoveEpisodesBefore(self):
+		now = datetime.date.today()
+		then = now - datetime.timedelta(3)
+
+		show2 = episoder.Show('B', url='b')
+		show2 = self.store.addShow(show2)
+
+		episode1 = episoder.Episode(self.show, 'e', 1, 1, now, 'x', 1)
+		episode2 = episoder.Episode(self.show, 'e', 1, 2, then, 'x', 1)
+		episode3 = episoder.Episode(show2, 'e', 1, 3, now, 'x', 1)
+		episode4 = episoder.Episode(show2, 'e', 1, 4, then, 'x', 1)
+
+		self.store.addEpisode(episode1)
+		self.store.addEpisode(episode2)
+		self.store.addEpisode(episode3)
+		self.store.addEpisode(episode4)
+		self.store.commit()
+
+		episodes = self.store.getEpisodes(basedate = then, n_days=10)
+		self.assertEqual(4, len(episodes))
+
+		show2.removeEpisodesBefore(self.store, now)
+
+		episodes = self.store.getEpisodes(basedate = then, n_days=10)
+		self.assertEqual(3, len(episodes))
+
+		self.show.removeEpisodesBefore(self.store, now)
+
+		episodes = self.store.getEpisodes(basedate = then, n_days=10)
+		self.assertEqual(2, len(episodes))
 
 class testEpguidesParser(unittest.TestCase):
 	def setUp(self):
