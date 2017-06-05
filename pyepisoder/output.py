2313c87a2359068c77e005c15350e21dcec99fef
Stefan Ott <stefan@ott.net>
2017-06-05 18:20:20 +0200

    Email notification cleanup

--- pyepisoder/output.py
+++ pyepisoder/output.py
@@ -17,9 +17,33 @@
 
 from sys import stdout
 from datetime import date, timedelta
+from smtplib import SMTP
 
 
-class ConsoleRenderer(object):
+class FormattingRenderer(object):
+
+	def __init__(self, format, dateformat):
+
+		self._format = format
+		self._dateformat = dateformat
+
+	def format(self, episode):
+
+		string = self._format
+
+		airdate = episode.airdate.strftime(self._dateformat)
+		string = string.replace("%airdate", airdate)
+		string = string.replace("%show", episode.show.name)
+		string = string.replace("%season", str(episode.season))
+		string = string.replace("%epnum", "%02d" % episode.episode)
+		string = string.replace("%eptitle", episode.title)
+		string = string.replace("%totalep", str(episode.totalnum))
+		string = string.replace("%prodnum", episode.prodnum)
+
+		return string
+
+
+class ConsoleRenderer(FormattingRenderer):
 
 	RED = "\033[31;1m"
 	CYAN = "\033[36;1m"
@@ -29,8 +53,7 @@ class ConsoleRenderer(object):
 
 	def __init__(self, format, dateformat, dest=stdout):
 
-		self._format = format
-		self._dateformat = dateformat
+		super(ConsoleRenderer, self).__init__(format, dateformat)
 		self._dest = dest
 
 	def __str__(self):
@@ -52,17 +75,7 @@ class ConsoleRenderer(object):
 
 	def _render(self, episode, render, color):
 
-		string = self._format
-		airdate = episode.airdate.strftime(self._dateformat)
-		string = string.replace("%airdate", airdate)
-		string = string.replace("%show", episode.show.name)
-		string = string.replace("%season", str(episode.season))
-		string = string.replace("%epnum", "%02d" % episode.episode)
-		string = string.replace("%eptitle", episode.title)
-		string = string.replace("%totalep", str(episode.totalnum))
-		string = string.replace("%prodnum", episode.prodnum)
-
-		render(string, color)
+		render(self.format(episode), color)
 
 	def render(self, episodes, use_colors=True):
 
@@ -89,3 +102,92 @@ class ConsoleRenderer(object):
 				color = ConsoleRenderer.GREY
 
 			self._render(episode, render, color)
+
+
+class NewEpisodesNotification(FormattingRenderer):
+
+	def __init__(self, episodes, format, datefmt):
+
+		super(NewEpisodesNotification, self).__init__(format, datefmt)
+		self._episodes = episodes
+
+	def __str__(self):
+
+		return "Notification: %s new episode(s)" % len(self._episodes)
+
+	def __repr__(self):
+
+		eps = "<(%d x Episode)" % len(self._episodes)
+		format = 'NewEpisodesNotification(%s, "%s", "%s")'
+		return format % (eps, self._format, self._dateformat)
+
+	def get(self):
+
+		return "".join("%s\n" % self.format(e) for e in self._episodes)
+
+	def send(self, notifier, to, pretend=False):
+
+		body = "Your upcoming episodes:\n\n"
+
+		for episode in self._episodes:
+			body += "%s\n" % self.format(episode)
+
+			if not pretend:
+				episode.notified = date.today()
+
+		notifier.send(body, to, pretend)
+
+
+class EmailNotifier(object):
+
+	def __init__(self, server, port, smtp=SMTP):
+
+		self._server = server
+		self._port = port
+		self._smtp = smtp
+		self._user = None
+		self._password = None
+
+		self.use_tls = False
+
+	def __str__(self):
+
+		return "EmailNotifier"
+
+	def __repr__(self):
+
+		format = 'EmailNotifier("%s", %d, %s)'
+		return format % (self._server, self._port, str(self._smtp))
+
+	def _get_header(self, to, subject):
+
+		return "From: %s\nTo: %s\nSubject: %s" % (to, to, subject)
+
+	def authenticate(self, user, password):
+
+		self._user = user
+		self._password = password
+
+	def send(self, body, to, pretend=False):
+
+		header = self._get_header(to, "Upcoming TV episodes")
+		message = "%s\n%s" % (header, body)
+
+		if pretend:
+			print(message)
+			return
+
+		server = self._smtp(self._server, self._port)
+
+		if self.use_tls:
+			server.starttls()
+		if self._user:
+			server.login(self._user, self._password)
+
+		server.sendmail(to, to, message)
+		server.quit()
+
+	def send_mail(self, message, to, auth=None, tls=False, pretend=False):
+
+		body = message.get()
+		self.send(body, to, auth, tls, pretend)
---
+++ test/notify.py
@@ -0,0 +1,300 @@
+# episoder, https://github.com/cockroach/episoder
+# -*- coding: utf8 -*-
+# # Copyright (C) 2004-2017 Stefan Ott. All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+
+from datetime import date
+from unittest import TestCase, TestSuite, TestLoader
+
+try:
+	from StringIO import StringIO
+except ImportError:
+	from io import StringIO
+
+from pyepisoder.database import Show, Episode
+from pyepisoder.output import EmailNotifier, NewEpisodesNotification
+
+
+class MockSMTPInstance(object):
+
+	instance = None
+
+	def __init__(self, server, port):
+
+		self.server = server
+		self.port = port
+		self.reset()
+
+	def reset(self):
+
+		self.count = 0
+		self.data = {}
+		self.tls = 0
+		self.user = None
+		self.password = None
+
+	def sendmail(self, from_addr, to_addr, msg):
+
+		self.data["from"] = from_addr
+		self.data["to"] = to_addr
+		self.data["msg"] = msg
+
+	def quit(self):
+
+		self.from_addr = self.data.get("from")
+		self.to_addr = self.data.get("to")
+		self.msg = self.data.get("msg")
+		self.count += 1
+
+	def starttls(self):
+
+		self.tls = 1
+
+	def login(self, user, password):
+
+		self.user = user
+		self.password = password
+
+
+class MockSMTP(object):
+
+	instance = None
+
+	def __init__(self, server, port):
+
+		if not MockSMTP.instance:
+			MockSMTP.instance = MockSMTPInstance(server, port)
+
+	def __str__(self):
+
+		return "MockSMTP"
+
+	def sendmail(self, from_addr, to_addr, msg):
+
+		self.instance.sendmail(from_addr, to_addr, msg)
+
+	def quit(self):
+
+		self.instance.quit()
+
+	def _get_count(self):
+
+		return MockSMTP.instance.count
+
+	def _set_count(self, num):
+
+		MockSMTP.instance.count = num
+
+	def _get_from(self):
+
+		return MockSMTP.instance.from_addr
+
+	def _get_to(self):
+
+		return MockSMTP.instance.to_addr
+
+	def _get_msg(self):
+
+		return MockSMTP.instance.msg
+
+	def _get_tls(self):
+
+		return MockSMTP.instance.tls
+
+	def _get_user(self):
+
+		return MockSMTP.instance.user
+
+	def _get_password(self):
+
+		return MockSMTP.instance.password
+
+	def starttls(self):
+
+		MockSMTP.instance.starttls()
+
+	def login(self, user, password):
+
+		MockSMTP.instance.login(user, password)
+
+	def reset(self):
+
+		MockSMTP.instance.reset()
+
+	count = property(_get_count, _set_count)
+	from_addr = property(_get_from)
+	to_addr = property(_get_to)
+	msg = property(_get_msg)
+	tls = property(_get_tls)
+	user = property(_get_user)
+	password = property(_get_password)
+
+
+class NotificationTest(TestCase):
+
+	def setUp(self):
+
+		self.show = Show(u"Test show 36")
+		self.show.show_id = 36
+
+		then = date(2008, 1, 1)
+		self.episode = Episode(u"Episode 41", 2, 5, then, u"NX01", 3)
+		self.episode.show = self.show
+
+		self.smtp = MockSMTP(None, None)
+		self.smtp.reset()
+
+	def test_str_and_repr(self):
+
+		notify = EmailNotifier("localhost", 25, MockSMTP)
+		self.assertEqual(str(notify), "EmailNotifier")
+		self.assertEqual(repr(notify), 'EmailNotifier("localhost", 25, '
+			'<class \'test.notify.MockSMTP\'>)')
+
+		message = NewEpisodesNotification([], "", "")
+		self.assertEqual(str(message), "Notification: 0 new episode(s)")
+		message = NewEpisodesNotification([None], "", "")
+		self.assertEqual(str(message), "Notification: 1 new episode(s)")
+		message = NewEpisodesNotification([None, None], "", "")
+		self.assertEqual(str(message), "Notification: 2 new episode(s)")
+
+		message = NewEpisodesNotification([None, None], "fmt", "Ymd")
+		expect = 'NewEpisodesNotification(<(2 x Episode), "fmt", "Ymd")'
+		self.assertEqual(repr(message), expect)
+
+	def test_send_mail_pretend(self):
+
+		show1 = Show(u"Columbo", u"")
+		ep1 = Episode(u"Episode 1", 1, 1, date(2014, 8, 10), u"x", 1)
+		ep2 = Episode(u"Episode 2", 1, 2, date(2014, 12, 1), u"x", 2)
+
+		ep1.show = show1
+		ep2.show = show1
+
+		stdout = sys.stdout
+		sys.stdout = StringIO()
+
+		message = NewEpisodesNotification([ep1, ep2],
+			"[%airdate] %show %seasonx%epnum - %eptitle","%Y-%m-%d")
+		notifier = EmailNotifier("localhost", 97, MockSMTP)
+		message.send(notifier, "xy@example.org", pretend=True)
+
+		self.assertEqual(self.smtp.count, 0)
+		output = sys.stdout.getvalue()
+
+		sys.stdout = stdout
+
+		# StringIO adds an extra \n
+		self.assertEqual(output,
+			"From: xy@example.org\n"
+			"To: xy@example.org\n"
+			"Subject: Upcoming TV episodes\n"
+			"Your upcoming episodes:\n\n"
+			"[2014-08-10] Columbo 1x01 - Episode 1\n"
+			"[2014-12-01] Columbo 1x02 - Episode 2\n\n")
+
+	def test_send_mail(self):
+
+		show1 = Show(u"Columbo", u"")
+		ep1 = Episode(u"Episode 1", 1, 1, date(2014, 8, 10), u"x", 1)
+		ep2 = Episode(u"Episode 2", 1, 2, date(2014, 12, 1), u"x", 2)
+
+		ep1.show = show1
+		ep2.show = show1
+
+		self.assertIsNone(ep1.notified)
+		self.assertIsNone(ep2.notified)
+
+		notifier = EmailNotifier("localhost", 97, MockSMTP)
+		message = NewEpisodesNotification([ep1, ep2],
+			"[%airdate] %show %seasonx%epnum - %eptitle","%Y-%m-%d")
+		message.send(notifier, "person@example.org")
+
+		self.assertEqual(self.smtp.count, 1)
+		self.assertEqual(self.smtp.tls, 0)
+		self.assertEqual(self.smtp.user, None)
+		self.assertEqual(self.smtp.password, None)
+		self.assertEqual(self.smtp.from_addr, "person@example.org")
+		self.assertEqual(self.smtp.to_addr, "person@example.org")
+		self.assertEqual(self.smtp.msg,
+			"From: person@example.org\n"
+			"To: person@example.org\n"
+			"Subject: Upcoming TV episodes\n"
+			"Your upcoming episodes:\n\n"
+			"[2014-08-10] Columbo 1x01 - Episode 1\n"
+			"[2014-12-01] Columbo 1x02 - Episode 2\n")
+
+		self.assertIsNotNone(ep1.notified)
+		self.assertIsNotNone(ep2.notified)
+
+	def test_send_mail_tls(self):
+
+		show1 = Show(u"Columbo", u"")
+		ep1 = Episode(u"Episode 1", 1, 1, date(2014, 8, 10), u"x", 1)
+		ep2 = Episode(u"Episode 2", 1, 2, date(2014, 12, 1), u"x", 2)
+
+		ep1.show = show1
+		ep2.show = show1
+
+		message = NewEpisodesNotification([ep1, ep2],
+			"[%airdate] %show %seasonx%epnum - %eptitle","%Y-%m-%d")
+
+		notifier = EmailNotifier("localhost", 97, MockSMTP)
+		notifier.use_tls = True
+		message.send(notifier, "xy@example.org")
+
+		self.assertEqual(self.smtp.count, 1)
+		self.assertEqual(self.smtp.tls, 1)
+		self.assertEqual(self.smtp.user, None)
+		self.assertEqual(self.smtp.password, None)
+		self.assertEqual(self.smtp.from_addr, "xy@example.org")
+		self.assertEqual(self.smtp.to_addr, "xy@example.org")
+		self.assertEqual(self.smtp.msg,
+			"From: xy@example.org\n"
+			"To: xy@example.org\n"
+			"Subject: Upcoming TV episodes\n"
+			"Your upcoming episodes:\n\n"
+			"[2014-08-10] Columbo 1x01 - Episode 1\n"
+			"[2014-12-01] Columbo 1x02 - Episode 2\n")
+
+	def test_send_mail_auth(self):
+
+		message = NewEpisodesNotification([], "", "")
+
+		notifier = EmailNotifier("localhost", 97, MockSMTP)
+		notifier.authenticate("someuser", "somepass")
+		message.send(notifier, "xy@example.org")
+
+		self.assertEqual(self.smtp.count, 1)
+		self.assertEqual(self.smtp.tls, 0)
+		self.assertEqual(self.smtp.user, "someuser")
+		self.assertEqual(self.smtp.password, "somepass")
+		self.assertEqual(self.smtp.from_addr, "xy@example.org")
+		self.assertEqual(self.smtp.to_addr, "xy@example.org")
+		self.assertEqual(self.smtp.msg,
+			"From: xy@example.org\n"
+			"To: xy@example.org\n"
+			"Subject: Upcoming TV episodes\n"
+			"Your upcoming episodes:\n\n")
+
+
+def test_suite():
+
+	suite = TestSuite()
+	loader = TestLoader()
+	suite.addTests(loader.loadTestsFromTestCase(NotificationTest))
+	return suite
